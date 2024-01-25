9fe6fe2fdd1ea005cb07eaec8dffd08d61a3f8a6
Huy Dinh Tran <91640334+huydinhtran@users.noreply.github.com>
2024-01-25 17:18:43 -0600

    Add files via upload

---
+++ server/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export CASSANDRA_HOME=/opt/cassandra
+export CASSANDRA_CONFIG=/etc/cassandra
+export PATH=$PATH:/opt/cassandra/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
